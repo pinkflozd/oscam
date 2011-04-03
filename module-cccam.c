pinkflozd/oscam

commit f6aef599f64b9c8e5b8cc53f0b0ecd6cd9575368

Replace all occurences of strcpy and sprintf by their safe variants (please report in the forum if anything broke by this revision!). Prevent future usage of these functions through globals.h
git-svn-id: http://www.oscam.cc/svn/oscam-mirror/trunk@4896 9dab235a-647b-404a-b20e-d39061ee4083

module-cccam.c
@@ -1690,7 +1690,7 @@ void addParam(char *param, char *value)
 
 int cc_parse_msg(struct s_client *cl, uint8 *buf, int l) {
 	struct s_reader *rdr = (cl->typ == 'c') ? NULL : cl->reader;
-
+	uint8 token[256];
 	int ret = buf[1];
 	struct cc_data *cc = cl->cc;
 
@@ -1739,10 +1739,10 @@ int cc_parse_msg(struct s_client *cl, uint8 *buf, int l) {
 			}
 			//Trick: when discovered partner is an Oscam Client, then we send him our version string:
 			if (cc->is_oscam_cccam) {
-				sprintf((char*) buf,
+				snprintf((char *)token, sizeof((char *)token),
 						"PARTNER: OSCam v%s, build #%s (%s) [EXT,SID]", CS_VERSION,
 						CS_SVN_VERSION, CS_OSTYPE);
-				cc_cmd_send(cl, buf, strlen((char*) buf) + 1, MSG_CW_NOK1);
+				cc_cmd_send(cl, token, strlen((char *)token) + 1, MSG_CW_NOK1);
 			}
 
 			cc->cmd05_mode = MODE_PLAIN;
@@ -1907,7 +1907,7 @@ int cc_parse_msg(struct s_client *cl, uint8 *buf, int l) {
 					if (!has_param)
 						param[0] = 0;
 					else {
-						strcpy(param, " [");
+						cs_strncpy(param, " [", sizeof(param));
 						if (cc->extended_mode)
 							addParam(param, "EXT");
 						if (cc->cccam220)
@@ -1915,9 +1915,10 @@ int cc_parse_msg(struct s_client *cl, uint8 *buf, int l) {
 						strcat(param, "]");
 					}
 
-					sprintf((char*) buf, "PARTNER: OSCam v%s, build #%s (%s)%s",
+					snprintf((char *)token, sizeof((char *)token),
+						"PARTNER: OSCam v%s, build #%s (%s)%s",
 						CS_VERSION, CS_SVN_VERSION, CS_OSTYPE, param);
-					cc_cmd_send(cl, buf, strlen((char*) buf) + 1, MSG_CW_NOK1);
+					cc_cmd_send(cl, token, strlen((char *)token) + 1, MSG_CW_NOK1);
 				}
 			}
 			return ret;
@@ -2292,13 +2293,13 @@ int cc_parse_msg(struct s_client *cl, uint8 *buf, int l) {
 		if (cl->typ == 'c') //client connection
 		{
 			//switching to an oder version and then disconnect...
-			strcpy(cfg.cc_version, version[0]);
+			cs_strncpy(cfg.cc_version, version[0], sizeof(cfg.cc_version));
 			ret = -1;
 		}
 		else //reader connection
 		{
-			strcpy(cl->reader->cc_version, version[0]);
-			strcpy(cl->reader->cc_build, build[0]);
+			cs_strncpy(cl->reader->cc_version, version[0], sizeof(cl->reader->cc_version));
+			cs_strncpy(cl->reader->cc_build, build[0], sizeof(cl->reader->cc_build));
 			cc_cli_close(cl, TRUE);
 		}
 		break;
@@ -2671,7 +2672,7 @@ int cc_srv_connect(struct s_client *cl) {
 
 	if (!cc->prefix)
 		cc->prefix = cs_malloc(&cc->prefix, strlen(cl->account->usr)+20, QUITERROR);
-	sprintf(cc->prefix, "cccam(s) %s: ", cl->account->usr);
+	snprintf(cc->prefix, strlen(cl->account->usr)+20, "cccam(s) %s: ", cl->account->usr);
 	
 
 	//Starting readers to get cards:
@@ -2872,7 +2873,7 @@ int cc_cli_connect(struct s_client *cl) {
 	}
 	if (!cc->prefix)
 		cc->prefix = cs_malloc(&cc->prefix, strlen(cl->reader->label)+20, QUITERROR);
-	sprintf(cc->prefix, "cccam(r) %s: ", cl->reader->label);
+	snprintf(cc->prefix, strlen(cl->reader->label)+20, "cccam(r) %s: ", cl->reader->label);
 
 	cc->ecm_counter = 0;
 	cc->max_ecms = 0;
@@ -3138,7 +3139,7 @@ void cc_cleanup(struct s_client *cl) {
 }
 
 void module_cccam(struct s_module *ph) {
-	strcpy(ph->desc, "cccam");
+	cs_strncpy(ph->desc, "cccam", sizeof(ph->desc));
 	ph->type = MOD_CONN_TCP;
 	ph->logtxt = ", crypted";
 	ph->watchdog = 1;

oscam-http.c
@@ -884,7 +884,7 @@ char *send_oscam_reader_config(struct templatevars *vars, struct uriparams *para
 		ll_append(configured_readers, newrdr);
 		newrdr->next = NULL; // terminate list
 		newrdr->enable = 0; // do not start the reader because must configured before
-		strcpy(newrdr->pincode, "none");
+		cs_strncpy(newrdr->pincode, "none", sizeof(newrdr->pincode));
 		for (i = 1; i < CS_MAXCAIDTAB; newrdr->ctab.mask[i++] = 0xffff);
 		for (i = 0; i < (*params).paramcount; ++i) {
 			if (strcmp((*params).params[i], "action"))

cscrypt/bn_lib.c
@@ -149,10 +149,10 @@ char *BN_options(void)
 		{
 		init++;
 #ifdef BN_LLONG
-		sprintf(data,"bn(%d,%d)",(int)sizeof(BN_ULLONG)*8,
+		snprintf(data, sizeof(data), "bn(%d,%d)",(int)sizeof(BN_ULLONG)*8,
 			(int)sizeof(BN_ULONG)*8);
 #else
-		sprintf(data,"bn(%d,%d)",(int)sizeof(BN_ULONG)*8,
+		snprintf(data, sizeof(data), "bn(%d,%d)",(int)sizeof(BN_ULONG)*8,
 			(int)sizeof(BN_ULONG)*8);
 #endif
 		}

cscrypt/bn_print.c
@@ -140,12 +140,12 @@ char *BN_bn2dec(const BIGNUM *a)
 		/* We now have a series of blocks, BN_DEC_NUM chars
 		 * in length, where the last one needs truncation.
 		 * The blocks need to be reversed in order. */
-		sprintf(p,BN_DEC_FMT1,*lp);
+		snprintf(p,num+3-(p-buf),BN_DEC_FMT1,*lp);
 		while (*p) p++;
 		while (lp != bn_data)
 			{
 			lp--;
-			sprintf(p,BN_DEC_FMT2,*lp);
+			snprintf(p,num+3-(p-buf),BN_DEC_FMT2,*lp);
 			while (*p) p++;
 			}
 		}

csctapi/icc_async.c
@@ -671,29 +671,29 @@ static int Parse_ATR (struct s_reader * reader, ATR * atr, unsigned short deprec
 		for (i=1; i<= numprot; i++) {
 			point = 0;
 			if (ATR_GetInterfaceByte (atr, i, ATR_INTERFACE_BYTE_TA, &tx) == ATR_OK) {
-				sprintf((char *)txt+point,"TA%i=%02X ",i,tx);
+				snprintf((char *)txt+point,sizeof(txt)-point,"TA%i=%02X ",i,tx);
 				point +=7;
 			}
 			if (ATR_GetInterfaceByte (atr, i, ATR_INTERFACE_BYTE_TB, &tx) == ATR_OK) {
-				sprintf((char *)txt+point,"TB%i=%02X ",i,tx);
+				snprintf((char *)txt+point,sizeof(txt)-point,"TB%i=%02X ",i,tx);
 				point +=7;
 			}
 			if (ATR_GetInterfaceByte (atr, i, ATR_INTERFACE_BYTE_TC, &tx) == ATR_OK) {
-				sprintf((char *)txt+point,"TC%i=%02X ",i,tx);
+				snprintf((char *)txt+point,sizeof(txt)-point,"TC%i=%02X ",i,tx);
 				point +=7;
 			}
 			if (ATR_GetInterfaceByte (atr, i, ATR_INTERFACE_BYTE_TD, &tx) == ATR_OK) {
-				sprintf((char *)txt+point,"TD%i=%02X ",i,tx);
+				snprintf((char *)txt+point,sizeof(txt)-point,"TD%i=%02X ",i,tx);
 				point +=7;
 				tx &= 0X0F;
-				sprintf((char *)txt+point,"(T%i)",tx);
+				snprintf((char *)txt+point,sizeof(txt)-point,"(T%i)",tx);
 				if (tx == 14)
 					OffersT[2] = TRUE;
 				else
 					OffersT[tx] = TRUE;
 			}
 			else {
-				sprintf((char *)txt+point,"no TD%i means T0",i);
+				snprintf((char *)txt+point,sizeof(txt)-point,"no TD%i means T0",i);
 				OffersT[0] = TRUE;
 			}
 			cs_debug_mask(D_ATR, "%s",txt);

csctapi/ifd_phoenix.c
@@ -341,7 +341,7 @@ static int stapi_writesettings(struct s_reader *reader, unsigned long ETU, unsig
 
 void cardreader_stapi(struct s_cardreader *crdr)
 {
-	strcpy(crdr->desc, "stapi");
+	cs_strncpy(crdr->desc, "stapi", sizeof(crdr->desc));
 	crdr->reader_init	= stapi_init;
 	crdr->get_status	= stapi_getstatus;
 	crdr->activate	= stapi_reset;
@@ -357,7 +357,7 @@ void cardreader_stapi(struct s_cardreader *crdr)
 
 void cardreader_mouse(struct s_cardreader *crdr) 
 {
-	strcpy(crdr->desc, "mouse_test");
+	cs_strncpy(crdr->desc, "mouse_test", sizeof(crdr->desc));
 	crdr->reader_init	= mouse_init;
 	crdr->get_status	= Phoenix_GetStatus;
 	crdr->activate	= Phoenix_Reset;

csctapi/ifd_smargo.c
@@ -181,7 +181,7 @@ static int smargo_transmit(struct s_reader *reader, unsigned char *sent, unsigne
 
 void cardreader_smargo(struct s_cardreader *crdr) 
 {
-	strcpy(crdr->desc, "smargo");
+	cs_strncpy(crdr->desc, "smargo", sizeof(crdr->desc));
 	crdr->reader_init	= smargo_init;
 	crdr->get_status	= Phoenix_GetStatus;
 	crdr->activate	= smargo_reset;

globals.h
@@ -34,6 +34,10 @@
 //for reader-nagra variables in s_reader:
 #include "cscrypt/idea.h"
 
+// Prevent use of unsafe functions
+#define strcpy(a,b) UNSAFE_STRCPY_USE_CS_STRNCPY_INSTEAD()
+#define sprintf(a,...) UNSAFE_SPRINTF_USE_SNPRINTF_INSTEAD()
+
 #ifndef CS_GLOBALS
 #define CS_GLOBALS
 #define CS_VERSION    "1.00-unstable_svn"

commit 00015c4039a0dbd4ff01a95d664b5b1b4a1da02e

WebIf: User status (online/connected) now depends from lastecm/login so that keepalive-packets are not respected anymore. Furthermore, cwrate is now calculated correct (earliest login over all clients/(cwok+cwnot+cwcache)) if the account has multiple connections. Furthermore fix a bug introduced in r4896 preventing cccam ext to work.
git-svn-id: http://www.oscam.cc/svn/oscam-mirror/trunk@4901 9dab235a-647b-404a-b20e-d39061ee4083

module-cccam.c
@@ -1739,7 +1739,7 @@ int cc_parse_msg(struct s_client *cl, uint8 *buf, int l) {
 			}
 			//Trick: when discovered partner is an Oscam Client, then we send him our version string:
 			if (cc->is_oscam_cccam) {
-				snprintf((char *)token, sizeof((char *)token),
+				snprintf((char *)token, sizeof(token),
 						"PARTNER: OSCam v%s, build #%s (%s) [EXT,SID]", CS_VERSION,
 						CS_SVN_VERSION, CS_OSTYPE);
 				cc_cmd_send(cl, token, strlen((char *)token) + 1, MSG_CW_NOK1);
@@ -1915,7 +1915,7 @@ int cc_parse_msg(struct s_client *cl, uint8 *buf, int l) {
 						strcat(param, "]");
 					}
 
-					snprintf((char *)token, sizeof((char *)token),
+					snprintf((char *)token, sizeof(token),
 						"PARTNER: OSCam v%s, build #%s (%s)%s",
 						CS_VERSION, CS_SVN_VERSION, CS_OSTYPE, param);
 					cc_cmd_send(cl, token, strlen((char *)token) + 1, MSG_CW_NOK1);

oscam-http.c
@@ -1670,7 +1670,7 @@ char *send_oscam_user_config(struct templatevars *vars, struct uriparams *params
 	/* List accounts*/
 	char *status, *expired, *classname, *lastchan;
 	time_t now = time((time_t)0);
-	int isec = 0, isconnected = 0;
+	int isec = 0;
 
 	char *filter = NULL;
 	int clientcount = 0;
@@ -1681,7 +1681,7 @@ char *send_oscam_user_config(struct templatevars *vars, struct uriparams *params
 	for (account=cfg.account; (account); account=account->next) {
 		//clear for next client
 		status = "offline"; lastchan = "&nbsp;", expired = ""; classname = "offline";
-		isconnected = 0; isec = 0;
+		isec = 0;
 
 		if(account->expirationdate && account->expirationdate < time(NULL)) {
 			expired = " (expired)";
@@ -1701,35 +1701,41 @@ char *send_oscam_user_config(struct templatevars *vars, struct uriparams *params
 			tpl_addVar(vars, TPLADDONCE, "SWITCH", "disable");
 		}
 
-		int lastresponsetm = 0;
+		int lastresponsetm = 0, latestactivity=0, earliestlogin=now;
 		char *proto = "";
-		float cwrate = 0;
+		double cwrate = 0.0;
 
 		//search account in active clients
 		int isactive = 0;
-		struct s_client *cl;
+		struct s_client *cl, *latestclient=NULL;
 		for (cl=first_client; cl ; cl=cl->next) {
-			if (cl->account && !strcmp(cl->account->usr, account->usr)) {
-				isconnected = 1;
-
-				if (!isactive)
-					status = "<b>connected</b>"; classname = "connected";
-
-				isec = now - cl->last;
-				if(isec < cfg.mon_hideclient_to) {
-					proto = monitor_get_proto(cl);
-					status = "<b>online</b>";
-					classname = "online";
-					lastchan = xml_encode(vars, get_servicename(cl->last_srvid, cl->last_caid));
-					lastresponsetm = cl->cwlastresptime;
-					isactive++;
-				}
-
-				if (cl->cwfound + cl->cwnot > 0) {
-					cwrate = cl->last - cl->login;
-					cwrate /= (cl->cwfound + cl->cwnot + cl->cwcache);
-				}
+			if (cl->account && !strcmp(cl->account->usr, account->usr)) {				
+				if(cl->lastecm > latestactivity || cl->login > latestactivity){
+					if(cl->lastecm > cl->login) latestactivity = cl->lastecm;
+					else latestactivity = cl->login;
+					latestclient = cl;
+				}				
+				if(cl->login < earliestlogin) earliestlogin = cl->login;		
 			}
+		}
+		
+		if(latestactivity > 0){
+			isec = now - latestactivity;				
+			if(isec < cfg.mon_hideclient_to) {
+				proto = monitor_get_proto(latestclient);
+				status = "<b>online</b>";
+				classname = "online";
+				lastchan = xml_encode(vars, get_servicename(latestclient->last_srvid, latestclient->last_caid));
+				lastresponsetm = latestclient->cwlastresptime;
+			} else if(latestclient != NULL) {
+				status = "<b>connected</b>";
+				classname = "connected";
+			}
+		}
+		
+		if (account->cwfound + account->cwnot + account->cwcache > 0) {
+			cwrate = now - earliestlogin;
+			cwrate /= (account->cwfound + account->cwnot + account->cwcache);
 		}
 
 		tpl_printf(vars, TPLADDONCE, "CWOK", "%d", account->cwfound);
@@ -2134,17 +2140,19 @@ char *send_oscam_status(struct templatevars *vars, struct uriparams *params, str
 						user_count_active++;
 						tpl_addVar(vars, TPLADD, "CLIENTTYPE", "a");
 					} else tpl_addVar(vars, TPLADD, "CLIENTTYPE", "c");
+					if(cl->lastecm > cl->login) isec = now - cl->lastecm;
+					else isec = now - cl->login;
 				} else {
 					if (cl->typ=='r' && cl->reader->card_status==CARD_INSERTED)
 						reader_count_conn++;
 					else if (cl->typ=='p' && (cl->reader->card_status==CARD_INSERTED ||cl->reader->tcp_connected))
 						proxy_count_conn++;
 					tpl_printf(vars, TPLADD, "CLIENTTYPE", "%c", cl->typ);
+					isec = now - cl->last;
 				}
 			
 				shown = 1;
 				lsec=now-cl->login;
-				isec=now-cl->last;
 				usr=username(cl);
 
 				if ((cl->typ=='r') || (cl->typ=='p')) usr=cl->reader->label;